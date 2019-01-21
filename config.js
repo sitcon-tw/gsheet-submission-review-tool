1b844dc936343de3386d9134113351b8660e9cde
Denny Huang <denny0223@gmail.com>
2019-01-21 22:00:17 +0800

    Config for SITCON 2019

--- config.js
+++ config.js
@@ -1,14 +1,16 @@
 window.CONFIG = {
   // dataFileName: 'demo-submission.html',
   fields: [
-    { name: 'speaker', label: 'Speaker' },
-    { name: 'bio', label: 'Bio' },
-    null,
-    { name: 'type', label: 'Talk Type' },
-    { name: 'abstract', label: 'Abstract' },
-    { name: 'description', label: 'Description' },
-    { name: 'subtype', label: 'Subject Type' },
-    { name: 'attachment', label: 'Attachment', isURL: true }
+    { name: '投稿時間', label: '投稿時間'},
+    { name: '投稿者', label: '投稿者'},
+    { name: '類型', label: '類型'},
+    { name: '主題', label: '主題'},
+    { name: '摘要', label: '摘要'},
+    { name: '大綱', label: '大綱'},
+    { name: '目標受眾', label: '目標受眾'},
+    { name: '詳細說明', label: '詳細說明'},
+    { name: '關鍵字', label: '關鍵字'},
+    { name: '附件連結', label: '附件連結', isURL: true}
   ],
   skipRows: 3
 }
