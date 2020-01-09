05f136b5a6f4f2cefbc29b7e6c4076b2800ba52c
koru1130 <m@koru.me>
2020-01-09 18:36:13 +0800

    fix wrong field name for 2020

--- config.js
+++ config.js
@@ -1,17 +1,21 @@
 window.CONFIG = {
   // dataFileName: 'demo-submission.html',
   fields: [
-    { name: '投稿時間', label: '投稿時間'},
-    { name: '投稿者', label: '投稿者'},
-    { name: '類型', label: '類型'},
-    { name: '主題', label: '主題'},
-    { name: '摘要', label: '摘要'},
-    { name: '大綱', label: '大綱'},
-    { name: '目標受眾', label: '目標受眾'},
-    { name: '先備知識', label: '先備知識'},
-    { name: '詳細說明', label: '詳細說明'},
-    { name: '關鍵字', label: '關鍵字'},
-    { name: '附件連結', label: '附件連結'}
+    { name: '時間戳記', label: '時間戳記'},
+    { name: '您的姓名 / 暱稱 Your Name / Nickname', label: '姓名 / 暱稱'},
+    { name: '您的 Email', label: 'Email'},
+    { name: '您的電話號碼 Your Phone Number', label: '電話號碼'},
+    { name: '您其他的聯絡方式 Anything else we can contact you', label: '其他聯絡方式'},
+    { name: '投稿類型', label: '投稿類型'},
+    { name: '題目 Title', label: '題目'},
+    { name: '摘要 Abstract', label: '摘要'},
+    { name: '演講大綱 Outline', label: '演講大綱'},
+    { name: '目標受眾 Target Audience', label: '目標受眾'},
+    { name: '先備知識 Prior Knowledge', label: '先備知識'},
+    { name: '詳細說明 Description', label: '詳細說明'},
+    { name: '關鍵字 Key Words', label: '關鍵字'},
+    { name: '附件 Attachment', label: '附件'},
+    { name: '電子郵件地址', label: '電子郵件地址'}
   ],
   skipRows: 3
 }
